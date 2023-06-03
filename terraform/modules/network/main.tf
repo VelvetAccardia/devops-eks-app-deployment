830dbc7ed149d4d72b4b0c9777b8e4eed21b75f4
Velvet <velvet@pop-os.localdomain>
2023-06-03 15:51:57 -0300

    finished network config

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -34,8 +34,39 @@ resource "aws_subnet" "sub_pvt_us_west_1" {
 resource "aws_route_table" "rt_pub" {
   vpc_id = aws_vpc.main.id
 
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.ig_pub.id
+  }
+
+  tags = {
+    Name = "rt_pub"
+  }
+
 }
 
 resource "aws_route_table" "rt_pvt" {
   vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "rt_pvt"
+  }
+}
+
+resource "aws_route_table_association" "pub_association" {
+  subnet_id      = aws_subnet.sub_pub_us_west_1.id
+  route_table_id = aws_route_table.rt_pub.id
+}
+
+resource "aws_route_table_association" "pvt_association" {
+  subnet_id      = aws_subnet.sub_pvt_us_west_1.id
+  route_table_id = aws_route_table.rt_pvt.id
+}
+
+resource "aws_internet_gateway" "ig_pub" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "ig_pub"
+  }
 }
