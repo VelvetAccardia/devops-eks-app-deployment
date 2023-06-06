f5af8baed831607a0bd952c4c94b6ff73e5059f6
Velvet <velvet@pop-os.localdomain>
2023-06-06 12:15:06 -0300

    fix: java app

--- app/spring-helloworld/src/main/java/com/velvetaccardia/springhelloworld/HelloWorldController.java
+++ app/spring-helloworld/src/main/java/com/velvetaccardia/springhelloworld/HelloWorldController.java
@@ -2,7 +2,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloController {
+public class HelloWorldController {
     @GetMapping("/")
     public String hello() {
         return "Hello World!";
