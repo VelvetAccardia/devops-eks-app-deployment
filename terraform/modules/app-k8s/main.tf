5d4a31ed0106c50375a4dce336b868378ea24d12
Velvet <velvet@pop-os.localdomain>
2023-06-06 11:57:00 -0300

    feat: new terraform pipeline

--- terraform/modules/app-k8s/main.tf
+++ terraform/modules/app-k8s/main.tf
@@ -1,39 +1,3 @@
-resource "kubernetes_deployment" "hello_world_deployment" {
-  metadata {
-    name = "spring-helloworld"
-  }
-
-  spec {
-    replicas = 1
-
-    selector {
-      match_labels = {
-        "app.kubernetes.io/name" = "spring-helloworld"
-      }
-    }
-
-    template {
-      metadata {
-        labels = {
-          "app.kubernetes.io/name" = "spring-helloworld"
-        }
-      }
-
-      spec {
-        container {
-          image = "velvetaccardia/spring-helloworld"
-          name  = "hello-world"
-
-          port {
-            name           = "tcp"
-            container_port = 8080
-          }
-        }
-      }
-    }
-  }
-}
-
 resource "kubernetes_service" "hello_world_svc" {
   metadata {
     name = "hello-world"
@@ -41,7 +5,7 @@ resource "kubernetes_service" "hello_world_svc" {
 
   spec {
     selector = {
-      app = "hello-world"
+      "app.kubernetes.io/name" = "spring-helloworld"
     }
 
     port {
