6da319c20c9278be16566b17c0e785aeb5a427cc
Velvet <velvet@pop-os.localdomain>
2023-06-06 12:11:26 -0300

    fix: java app

--- terraform/modules/app-k8s/main.tf
+++ terraform/modules/app-k8s/main.tf
@@ -1,3 +1,39 @@
+resource "kubernetes_deployment" "hello_world_deployment" {
+  metadata {
+    name = "spring-helloworld"
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        "app.kubernetes.io/name" = "spring-helloworld"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          "app.kubernetes.io/name" = "spring-helloworld"
+        }
+      }
+
+      spec {
+        container {
+          image = "velvetaccardia/spring-helloworld"
+          name  = "hello-world"
+
+          port {
+            name           = "tcp"
+            container_port = 8080
+          }
+        }
+      }
+    }
+  }
+}
+
 resource "kubernetes_service" "hello_world_svc" {
   metadata {
     name = "hello-world"
